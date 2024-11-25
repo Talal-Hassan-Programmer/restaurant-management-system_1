100d45d2949590b5269d9bf4ac0145dbccbe0c19
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2024-11-25 21:43:08 +0300

    creating func.py for funcutions,, creating main.py for main.py as main python file

---
+++ func.py
@@ -0,0 +1,14 @@
+# all funcs for this projects 
+
+
+#adding a order 
+def add_to_order():
+    pass
+
+#removing a order
+def remove_order():
+    pass
+
+#creating a order file
+def create_order_file():
+    pass
